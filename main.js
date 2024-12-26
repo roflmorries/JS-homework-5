05caccd5b39f91e6b21bbb9f7f9adf5bcb2111ff
roflmorries <99510874+roflmorries@users.noreply.github.com>
2024-12-26 19:22:21 +0200

    some fixes

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ array.sort((a, b) => a - b);
 document.write(array.join("\n") + "<br>");
 
 array.splice(1,3)
-document.write(array.join("\n"));
+document.write(array.join("\n") + "<br>");
 
 
 // 2.
@@ -31,75 +31,33 @@ document.write(array.join("\n"));
 const userArray = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
 
-// a.
-
 let positiveSum = 0;
 let positiveCount = 0;
-
-for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i] > 0) {
-        positiveSum += userArray[i];
-        positiveCount++;
-    }
-}
-
-document.write(`Сумма положительных чисел: ${positiveSum}` + "<br>");
-document.write(`Количество положительных чисел: ${positiveCount}` + "<br>");
-
-
-// b.
 
 let minValue = userArray[0];
 let indexValue = 0;
 
-for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i] < minValue) {
-        minValue = userArray[i];
-        indexValue = i;
-    }
-}
-
-document.write(`Минимальный элемент массива: ${minValue}, его индекс: ${indexValue}` + "<br>");
-
-
-// c.
-
 let maxValue = userArray[0];
 let indexMaxValue = 0;
 
-for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i] > maxValue) {
-        maxValue = userArray[i];
-        indexMaxValue = i;
-    }
-}
-
-document.write(`Наибольший элемент массива: ${maxValue}, его индекс: ${indexMaxValue}` + "<br>");
-
-
-// d.
-
 let negativeCount = 0;
-
-
-for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i] < 0) {
-        negativeCount++;
-    }
-}
-
-document.write(`Количество отрицательных чисел элемента: ${negativeCount}` + "<br>");
-
-
-// e - h.
 
 let oddPositiveCount = 0;
 let evenPositiveCount = 0;
 let oddPositiveSum = 0;
 let evenPositiveSum = 0;
 
+let positiveProduct = 1;
+
+
+
 for (let i = 0; i < userArray.length; i++) {
+
     if (userArray[i] > 0) {
+        positiveSum += userArray[i];
+        positiveCount++;
+        positiveProduct *= userArray[i];
+
         if (userArray[i] % 2 === 0) {
             evenPositiveCount++;
             evenPositiveSum += userArray[i];
@@ -108,26 +66,21 @@ for (let i = 0; i < userArray.length; i++) {
             oddPositiveSum += userArray[i];
         }
     }
-}
 
-document.write(`Количество парных элементов: ${evenPositiveCount}, их сумма: ${evenPositiveSum}` + "<br>");
-document.write(`Количество непарных элементов: ${oddPositiveCount}, их сумма: ${oddPositiveSum}` + "<br>");
+    if (userArray[i] < 0) {
+        negativeCount++;
+    }
 
+    if (userArray[i] < minValue) {
+        minValue = userArray[i];
+        indexValue = i;
+    }
 
-// i.
-
-let positiveProduct = 1;
-
-for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i] > 0) {
-        positiveProduct *= userArray[i];
+    if (userArray[i] > maxValue) {
+        maxValue = userArray[i];
+        indexMaxValue = i;
     }
 }
-
-document.write(`Произведение всех положительных элементов: ${positiveProduct}` + "<br>");
-
-
-// j.
 
 
 let nullArray = [];
@@ -136,4 +89,34 @@ for (let i = 0; i < userArray.length; i++) {
     nullArray.push(i === indexMaxValue ? userArray[i] : 0);
 }
 
+
+// a.
+
+document.write(`Сумма положительных чисел: ${positiveSum}` + "<br>");
+document.write(`Количество положительных чисел: ${positiveCount}` + "<br>");
+
+// b.
+
+document.write(`Минимальный элемент массива: ${minValue}, его индекс: ${indexValue}` + "<br>");
+
+// c.
+
+document.write(`Наибольший элемент массива: ${maxValue}, его индекс: ${indexMaxValue}` + "<br>");
+
+// d.
+
+document.write(`Количество отрицательных чисел элемента: ${negativeCount}` + "<br>");
+
+// e - h.
+
+document.write(`Количество парных элементов: ${evenPositiveCount}, их сумма: ${evenPositiveSum}` + "<br>");
+document.write(`Количество непарных элементов: ${oddPositiveCount}, их сумма: ${oddPositiveSum}` + "<br>");
+
+// i.
+
+document.write(`Произведение всех положительных элементов: ${positiveProduct}` + "<br>");
+
+// j.
+
 document.write(nullArray.join(", "));
+
